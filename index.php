335bf42490e2305425080b66a3c196512e94fb29
PepijnIjnema <Pepijn14j@gmail.com>
2022-06-28 13:53:35 +0200

    footer en header html

--- index.php
+++ index.php
@@ -23,9 +23,14 @@
 </head>
 
 <body>
+  <!-- Header -->
+  <header>
+    <?php require_once 'header.php'; ?>
+  </header>
+  <!-- Eind van header -->
+  <!-- Hier komt onze code voor de body -->
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <!-- Eind code body-->
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
@@ -36,6 +41,11 @@
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async></script>
+  <!-- Footer -->
+  <footer>
+    <?php require_once 'footer.php'; ?> 
+  </footer>
+  <!-- Eind van footer -->
 </body>
-
+  
 </html>
