25e34dc88e56d4bb2badee7808bfbe1da1071647
PepijnIjnema <Pepijn14j@gmail.com>
2022-06-28 17:19:20 +0200

    start css header en footer

--- header.php
+++ header.php
@@ -11,12 +11,10 @@
 			<div class="navbar">
 				<a href="index.php">Home</a>
 				<a href="OverOns.php">Over ons</a>
-			</div>
-			<div class="navbar">
 				<?php if(isset($_SESSION['user_id'])): ?>
-				<a href="logout.php">Uitloggen</a>
+				<a class="login" href="logout.php">Uitloggen</a>
 				<?php else: ?>
-				<a href="login.php">Inloggen</a>
+				<a class="login" href="login.php">Inloggen</a>
 				
 				<?php endif; ?>
 			</div>
