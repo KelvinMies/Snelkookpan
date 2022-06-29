fc1d8139e234d94a0b5142c6fad35459b6160cea
PepijnIjnema <Pepijn14j@gmail.com>
2022-06-29 16:24:21 +0200

    Update admin.php

---
+++ admin.php
@@ -0,0 +1,14 @@
+ <?php session_start() ?>
+
+  <!-- Header -->
+  <header>
+    <?php require_once 'header.php'; ?>
+  </header>
+  <!-- Eind van header -->
+
+
+  <!-- Footer -->
+  <footer>
+    <?php require_once 'footer.php'; ?> 
+  </footer>
+  <!-- Eind van footer -->
