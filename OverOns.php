48d14eeddac6994ce5c9551e180f9f8c469152f6
PepijnIjnema <Pepijn14j@gmail.com>
2022-06-29 13:59:28 +0200

    over ons pagina

---
+++ OverOns.php
@@ -0,0 +1,45 @@
+<body>
+  <!-- Header -->
+  <header>
+    <?php require_once 'header.php'; ?>
+  </header>
+  <!-- Eind van header -->
+  <!-- Hier komt onze code voor de body -->
+<div class="container">
+  <main class="overons">
+    <div class="overons1">
+        <h1>Over ons</h1>
+        <p>Dagelijks verbinden wij vraag en aanbod op de vastgoedmarkt. We helpen kopers en verkopers, huurders en verhuurders, en makelaars op weg. En dat doen we al 20 jaar. Inmiddels zijn we een van de grootste websites van Nederland waar iedere maand gemiddeld 5 miljoen mensen samenkomen. Op zoek naar een nieuw thuis of plek voor zakelijke ambities. Of om even binnen te kijken bij de buren.</p>
+        <img src="img/overons.jpg">
+
+    </div>
+    <div class="overons2">
+      <h1>Voor iedereen die een huis zoekt of verkoopt</h1>
+      <p>Wij helpen jou tijdens een van de belangrijkste keuzes in je leven: het vinden of verkopen van een woning. Dat is spannend en niet altijd makkelijk. Daarom ondersteunen we je zoveel mogelijk in dit proces.</p>
+      <img src="img/deal.jpg">
+
+
+    </div>
+    <div class="overons3">
+      <h1>Voor alle makelaars</h1>
+      <p>Wij helpen je ondernemen via ons vastgoedplatform. Met data, inzichten en leads. We optimaliseren het platform en onze diensten continu zodat jij eenvoudig en effectief kunt werken.</p>
+      <img src="img/huizenmarkt.jpg">
+    </div>
+    <div class="overons4">
+      <h1>Voor iedereen op zoek naar bedrijfsruimte</h1>
+      <p>Wij helpen je om zakelijke dromen te realiseren. Dit doen we met funda in business. Hier komt het meest complete aanbod van (zakelijk) onroerend goed samen. Van centrumlocatie tot groothandel.</p>
+      <img src="img/ruimte.jpg">
+
+    </div>
+  </main>
+</div>
+  <!-- Eind code body-->
+
+  <!-- Footer -->
+  <footer>
+    <?php require_once 'footer.php'; ?> 
+  </footer>
+  <!-- Eind van footer -->
+</body>
+  
+</html>
