7ed718ddb186dcb7bc12c41f655dcc7afcd793ca
Silvin van Haestregt <D289981@edu.curio.nl>
2022-06-30 11:26:46 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    if ($_SESSION['user_admin'] == "1"){
-      header("Location: index.php");
+    if ($_SESSION['user_admin'] != 1){
+        header("location: index.php");
     }
 ?>
 
